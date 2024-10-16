9a457d7c499f237b705a57196aa7eb96d5efd54d
Kim Padua <paduakimberly13@gmail.com>
2024-10-16 16:08:50 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const totalUsersElement = document.getElementById('totalUsers');
+const totalSalesElement = document.getElementById('totalSales');
+const alertButton = document.getElementById('alertButton');
+
+alertButton.addEventListener('click', () => {
+    const totalUsers = totalUsersElement.textContent;
+    const totalSales = totalSalesElement.textContent;
+    alert(`Total Users: ${totalUsers}, Total Sales: ${totalSales}`);
+});
